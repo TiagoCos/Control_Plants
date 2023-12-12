aaaa63830ffdf9f811d94a3ff601cbe0fa0cacf4
Your NameTiagoCos <tiagocostasilva27@gmail.com>
2023-12-11 22:39:47 -0300

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react';
+
+const plants_Url = "https://plant.id/api/v3";
+const apiKey = "45xsL3fMn7u4LeG3OIM6v0Z2zAkTqSoeC6wzrWU5Nhwj7Io2IK";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [listPlants, setListPlants] = useState([]);
 
-export default Home
+  const getPlants = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setListPlants(data);
+    console.log(data)
+  };
+
+  useEffect(() => {
+    const Url_final = `https://servicos.jbrj.gov.br/flora/genus/pitaiya`;
+    getPlants(Url_final);
+  }, []); // vetor de condição vazio
+
+  return (
+    <div className='container'>
+      <div className="plants-container">
+         { /*<div>{listPlants.length && listPlants.map((plants) => <p>{plants.id}</p>)}</div>*/}
+      </div>
+      <p>{listPlants}</p>
+    </div>
+  );
+};
+
+export default Home;
