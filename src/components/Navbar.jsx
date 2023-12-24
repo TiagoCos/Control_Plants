7ac8435a85f8e47dccc9b9f6c542842303007d80
Your NameTiagoCos <tiagocostasilva27@gmail.com>
2023-12-23 21:17:54 -0300

    ...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import {BiSearchAlt2} from 'react-icons/bi';
 import { TbPlant2 } from "react-icons/tb";
+import './Navbar.css'
 
 function Navbar() {
   return (
-    <div>
-     <h2>
+    <div className='nav'>
+
+      <form className='pesquisa'>
+      <h2 >
       <Link to="/"> <TbPlant2/> Plants </Link>
      </h2>
-      <form>
         <input type='text' placeholder='Busque por uma planta'/>
       <button type='submit'><BiSearchAlt2/></button>
       </form>
